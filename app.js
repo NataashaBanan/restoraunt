bdf4a112e05b5f52cb34c0e0cd3969884695a627
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2021-01-19 00:32:11 +0500

    add pug and css for first page

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var menuRouter = require('./routes/menu');
+var contactsRouter = require('./routes/contacts')
+var salesRouter = require('./routes/sales')
+var workRouter = require('./routes/work')
 
 var app = express();
 
@@ -34,7 +37,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/menu', menuRouter);
+app.use('/contacts', contactsRouter);
+app.use('/sales', salesRouter);
+app.use('/work', workRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
-//menu page
-//contacts page
-//sales page
-//work page
-//TODO: add these pages
 
 var booking_controller = require('../controllers/bookingController')
 
