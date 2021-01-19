eec376ad644760aa5200fe5e7ee9fced831b00b3
NataashaBanan <49069370+NataashaBanan@users.noreply.github.com>
2021-01-19 13:28:54 +0500

    connect forms and http methods

--- routes/index.js
+++ routes/index.js
@@ -9,15 +9,13 @@ router.get('/', function(req, res, next) {
 
 var booking_controller = require('../controllers/bookingController')
 
-//GET request for creating booking
-router.get('/booking/create', booking_controller.booking_create_get);
 //POST request for creating booking
 router.post('/booking/create', booking_controller.booking_create_post);
-//GET request for deleting booking
-router.get('/booking/delete', booking_controller.booking_delete_get);
+
 //POST request for deleting booking
-router.get('/booking/delete', booking_controller.booking_delete_post);
+router.post('/booking/delete', booking_controller.booking_delete_post);
+
 //GET request for one booking
-router.get('/booking/:id', booking_controller.booking_detail);
+router.get('/booking', booking_controller.booking_detail);
 
 module.exports = router;
